34fd7ff092affaccfccef7a903ced52754a15346
hyo0508 <2018008168@hanyang.ac.kr>
2023-06-15 16:57:13 +0900

    feat(sort): quick-sort.py

--- python/6_Sort/quick-sort.py
+++ python/6_Sort/quick-sort.py
@@ -4,4 +4,22 @@ from typing import List
 
 class QuickSort(Strategy):
     def sort(self, nums: List) -> None:
-        pass
+        def _sort(low, high):
+            if high - low < 2:
+                return
+
+            pivot = _partition(low, high)
+            _sort(low, pivot)
+            _sort(pivot+1, high)
+
+        def _partition(low, high):
+            pivot = high - 1
+
+            for i in range(high - 2, -1, -1):
+                if nums[i] > nums[pivot]:
+                    nums[i], nums[pivot] = nums[pivot], nums[i]
+                    pivot = i
+
+            return pivot
+
+        _sort(0, len(nums))
